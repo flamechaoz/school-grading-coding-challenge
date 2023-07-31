cae487d1bf9c83fbb598afda6e3b687d351da413
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2023-07-31 17:06:03 +0800

    Add placeholder props for TextArea component

--- client/src/components/TextArea.jsx
+++ client/src/components/TextArea.jsx
@@ -1,7 +1,7 @@
-function TextArea ({ value, onChange }){
+function TextArea ({ onChange, placeholder, value }){
   return (
     <>
-      <textarea onChange={onChange} rows="10" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your thoughts here..." />
+      <textarea onChange={onChange} rows="10" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder={placeholder} />
     </>
   )
 }
